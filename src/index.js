6b9230e44cc6cdceb4b928ed833a09056a57371c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-11-30 15:38:25 -0300

    fixed linters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Index.css';
+import './index.css';
 import App from './App.js';
 
 ReactDOM.render(
