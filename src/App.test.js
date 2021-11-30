b3554f6861aa9a7340dbb519c4dc7375ea1dec13
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-11-30 15:15:08 -0300

    create calculator component

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-// eslint-disable-next-line no-unused-vars
-import App from './App.js';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/components/Calculator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+        <section >
+        <div className="container">
+            <div className="display">
+            <div className="display-2">0</div>
+                <div className="temp-result"></div>
+            </div>
+<div className="all-buttons">
+    <div className="btn all-clear">AC</div>
+    <div className="btn last-clear">+/-</div>
+    <div className="btn ">%</div>
+    <div className="btn operation">/</div>
+    <div className="btn number">7</div>
+    <div className="btn number">8</div>
+    <div className="btn number">9</div>
+    <div className="btn operation">X</div>
+    <div className="btn number">4</div>
+    <div className="btn number">5</div>
+    <div className="btn number">6</div>
+    <div className="btn operation">-</div>
+    <div className="btn number">1</div>
+    <div className="btn number">2</div>
+    <div className="btn number">3</div>
+    <div className="btn operation">+</div>
+    <div className="btn number zero">0</div>
+    <div className="btn number">.</div>
+    <div className="btn equal operation">=</div>
+
+</div>
+        </div>
+    </section>
+    );
+  }
+}
+export default Calculator;
