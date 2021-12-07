e73a7761267789d1688d36a366c2bc0a5092aaa6
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-12-07 20:36:40 -0300

    create routers and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <section>
+      <nav className="navbar">
+        <div className="nav-div">
+          <Link className="link" to="/">Home</Link>
+          <Link className="link" to="/calc">Calculator</Link>
+          <Link className="link" to="/quotes">Quotes</Link>
+        </div>
+        <h1 className="header"> Math Magicians </h1>
+      </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route path="/calc" element={<Calculator />} />
+
+        <Route path="/quotes" element={<Quote />} />
+
+      </Routes>
+    </section>
+  </>
+);
 
 export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+const Quote = () => (
+
+  <section>
+
+    <p className="quotes">
+      Pure mathematics is, in its way, the poetry of logical ideas.
+      — Albert Einstein, German theoretical physicist
+
+    </p>
+  </section>
+);
+export default Quote;
