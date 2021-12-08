a0439e1290330d76fa0c00a0ec75bad3199c5a44
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-12-08 20:41:03 -0300

    linter issues

--- src/components/logic/calculate.test.js
+++ src/components/logic/calculate.test.js
@@ -2,33 +2,32 @@ import calculate from './calculate';
 
 describe('Calculate', () => {
   const obj = {
-    total : null,
-    next:null,
-    operation : null,
+    total: null,
+    next: null,
+    operation: null,
 
-  }
+  };
   test('should return Object', () => {
     expect(typeof (calculate(obj, 'AC'))).toBe('object');
-  })
+  });
 
   test('if buttonName ==0 ,and obj.next==0', () => {
-    obj.next ='0';
+    obj.next = '0';
     expect(calculate(obj, '0')).toEqual({});
   });
 
   test('when buttonName is .', () => {
-    obj.next = '4.2'
-    const result = calculate(obj, '.')
+    obj.next = '4.2';
+    const result = calculate(obj, '.');
     expect(result.next).toBe('4.2');
   });
 
   test('when buttonName === "=", and obj.next == 0 and operation is null', () => {
-    
-    obj.total = '6'
-    obj.next = '3'
-    obj.operation = '+'
+    obj.total = '6';
+    obj.next = '3';
+    obj.operation = '+';
 
-    const solution = calculate(obj, '=')
+    const solution = calculate(obj, '=');
     expect(solution.total).toBe('9');
   });
-})
+});
