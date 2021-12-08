767ee10306b4e71622b16d3bd63aaf49cb3d554b
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-09 00:50:33 +0200

    Add changes to user interactive

--- src/components/Buttons.test.js
+++ src/components/Buttons.test.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Buttons from './Buttons';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react'
+
+const  handleClick = jest.fn()
 
 describe('Buttons', () => {
   test('renders Button component', () => {
@@ -9,3 +13,22 @@ describe('Buttons', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+
+// test('calls onClick prop when clicked', () => {
+//   const handleClick = jest.fn()
+//   render(<Buttons onClick={handleClick}>+</Buttons>)
+//   fireEvent.click(screen.getByText('+'))
+//   expect(handleClick).toHaveBeenCalledTimes(1)
+// })
+
+// describe('simulate user interaction', () => {
+ 
+  test('click button and simulate user interaction', ()=>{
+    const buttonName = '+';
+    const component = render(<Buttons name={ buttonName } onClick={ handleClick }/>)
+    const button = component.getByText('+')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent('+')
+  })
+// });
