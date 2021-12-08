240d27c1daa07fc1740e0008e77c170af5557655
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-09 01:03:20 +0200

    Added more test cases

--- src/components/Buttons.test.js
+++ src/components/Buttons.test.js
@@ -14,21 +14,34 @@ describe('Buttons', () => {
   });
 });
 
-
-// test('calls onClick prop when clicked', () => {
-//   const handleClick = jest.fn()
-//   render(<Buttons onClick={handleClick}>+</Buttons>)
-//   fireEvent.click(screen.getByText('+'))
-//   expect(handleClick).toHaveBeenCalledTimes(1)
-// })
-
-// describe('simulate user interaction', () => {
- 
   test('click button and simulate user interaction', ()=>{
     const buttonName = '+';
     const component = render(<Buttons name={ buttonName } onClick={ handleClick }/>)
     const button = component.getByText('+')
     fireEvent.click(button)
     expect(component.container).toHaveTextContent('+')
-  })
-// });
+  });
+
+  test('click button and simulate user interaction', ()=>{
+    const buttonName = '-';
+    const component = render(<Buttons name={ buttonName } onClick={ handleClick }/>)
+    const button = component.getByText('-')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent('-')
+  });
+
+  test('click button and simulate user interaction', ()=>{
+    const buttonName = '3';
+    const component = render(<Buttons name={ buttonName } onClick={ handleClick }/>)
+    const button = component.getByText('3')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent('3')
+  });
+
+  test('click button and simulate user interaction', ()=>{
+    const buttonName = '2';
+    const component = render(<Buttons name={ buttonName } onClick={ handleClick }/>)
+    const button = component.getByText('2')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent('2')
+  });
